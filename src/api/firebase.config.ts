b59dd02bb9766abf2d1123a023a3229c96f27df3
SerbianGhost <serbianghost816@gmail.com>
2024-02-25 00:58:43 +0600

    Create firebase.config.ts

---
+++ src/api/firebase.config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXVZl7kNbSorIU9_hA3MOUqbdEdc2K2D8",
+    authDomain: "locale-da5b5.firebaseapp.com",
+    projectId: "locale-da5b5",
+    storageBucket: "locale-da5b5.appspot.com",
+    messagingSenderId: "552773037223",
+    appId: "1:552773037223:web:fca3613f409ebab5f9ff82",
+    measurementId: "G-Q8LW8XJ7FC"
+  };
+
+export default firebaseConfig;
